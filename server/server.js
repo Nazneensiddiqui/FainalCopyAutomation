336aba268d4e48dd2f1b4230daa26652198f753a
nazneensiddiqui <nazneenbanosiddiqui@gmail.com>
2025-05-18 10:02:44 +0530

    final

--- server/server.js
+++ server/server.js
@@ -40,23 +40,9 @@ const app = express();
 
 
 //********************************************************************* */
-const allowedOrigins = [
-  "http://localhost:5174", // local frontend
-  "https://fainal-copy-automation-7jnf.vercel.app/" // deployed frontend
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true
-  })
-);
+app.use(cors({
+  origin: 'https://fainal-copy-automation-7jnf.vercel.app'  
+}));
 
 app.use(bodyParser.json());
 //********************************************************* */
